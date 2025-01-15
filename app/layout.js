b1cff69143c0db40cbc15663aed6e20d4e080345
ItsTulasiRaman <tulasiraman917@gmail.com>
2025-01-15 18:51:11 +0530

    Info updation

--- app/layout.js
+++ app/layout.js
@@ -10,7 +10,7 @@ const ovo = Ovo({
 });
 
 export const metadata = {
-  title: "Suryakumar Portfolio",
+  title: "Tulasi Raman Portfolio",
   description: "",
 };
 
--- components/Footer.jsx
+++ components/Footer.jsx
@@ -8,15 +8,15 @@ const Footer = ({isDarkMode}) => {
             <Image src={isDarkMode ? assets.logo_dark : assets.logo} alt='' className='w-36 mx-auto mb-2'/>
 
             <div className='w-max flex items-center gap-2 mx-auto'>
-                <Image src={isDarkMode ? assets.mail_icon_dark : assets.mail_icon} alt='' className='w-6'/>suryakumar.pandiaraj@gmail.com
+                <Image src={isDarkMode ? assets.mail_icon_dark : assets.mail_icon} alt='' className='w-6'/>tulasiraman.r@outlook.com
             </div>
         </div>
         <div className='text-center sm:flex items-center justify-between border-t border-gray-400 mx-[10%] mt-12 py-6'>
-            <p>© 2025 Suryakumar. All rights reserved</p>
+            <p>© 2025 Tulasi Raman. All rights reserved</p>
             <ul className='flex items-center gap-10 justify-center mt-4 sm:mt-0'>
-                <li><a target='_blank' href='https://github.com/Its-Suryakumar'>GitHub</a></li>
-                <li><a target='_blank' href='https://linkedin.com/in/suryakumarp'>LinkedIn</a></li>
-                <li><a target='_blank' href='https://x.com/Its_Suryakumar'>Twitter</a></li>
+                <li><a target='_blank' href='https://github.com/ItsTulasiRaman'>GitHub</a></li>
+                <li><a target='_blank' href='https://www.linkedin.com/in/tulasiramanr'>LinkedIn</a></li>
+                <li><a target='_blank' href='https://x.com/'>Twitter</a></li>
             </ul>
         </div>
     </div>
